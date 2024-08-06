ae370a861501426e01ab1de480c9481d7ec3f84e
sarafaman211 <94362952+sarafaman211@users.noreply.github.com>
2024-08-06 09:30:03 +0530

    Minor upgrades

--- docker-install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-sudo apt-get update
-sudo apt-get install -y  ca-certificates curl gnupg
-curl -fsSL https://get.docker.com -o install-docker.sh
-cat install-docker.sh
-sudo sh install-docker.sh
--- logs/start.sh
+++ logs/start.sh
@@ -1,4 +1,3 @@
-
 #! /bin/bash
 
 docker run --net=host  -d --restart=always  --name=log-dipatcher-1 -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME:$HOME  -v /media/vdata/logs/:/media/vdata/logs/ -w / -e DISPLAY=$DISPLAY --log-driver local --log-opt max-size=10m bitvividtac/itms:bitvivid-itms-logs-dispatcher-0.2 python3 dispatcher.py /media/vdata/logs/ http://192.168.100.92:4101/v1/logs/  1  http 192.168.100.92 9200 elastic changeme http://192.168.100.92:8080/realms/XconnectedAuthentication/protocol/openid-connect/token  admin@admin.com  admin123 nestjs-client 3GrdLrWRYDgUsd729N7LI4zs5PV4dFk6 json
