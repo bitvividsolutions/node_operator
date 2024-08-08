14e5bb3f4a1ea9c780ce686f71d0e491d83ba5f5
sarafaman211 <94362952+sarafaman211@users.noreply.github.com>
2024-08-08 18:32:45 +0530

    update the START.SH and DOCKER compose file

--- logs/start.sh
+++ logs/start.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-docker run --net=host  -d --restart=always   --name=log-dipatcher-1 -e DEBUG=1 -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME:$HOME  -v /media/vdata/logs/:/media/vdata/logs/ -w / -e DISPLAY=$DISPLAY --log-driver local --log-opt max-size=10m xconnected/components:itms-logs-dispatcher-0.3 python3 dispatcher.py /media/vdata/logs/ http://10.71.5.3/api-itms/v1/logs/ json http://10.71.5.3/api-itms/v1/specialVehicle/  http://10.71.5.3/auth/realms/XconnectedAuthentication/protocol/openid-connect/token  system  system@123 node NGEzna22kqqBDaAqj8lvsiEWZ4jhZrNv
+docker run --net=host  -d --restart=always   --name=log-dipatcher-1 -e DEBUG=1 -e TZ=Asia/Kolkata -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME:$HOME  -v /media/vdata/logs/:/media/vdata/logs/ -w / -e DISPLAY=$DISPLAY --log-driver local --log-opt max-size=10m xconnected/components:itms-logs-dispatcher-0.3 python3 dispatcher.py /media/vdata/logs/ http://10.71.5.3/api-itms/v1/logs/ json http://10.71.5.3/api-itms/v1/specialVehicle/  http://10.71.5.3/auth/realms/XconnectedAuthentication/protocol/openid-connect/token  system  system@123 node NGEzna22kqqBDaAqj8lvsiEWZ4jhZrNv
+
 
 
 
